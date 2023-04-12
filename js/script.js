ac870d8d69570acccf65592970cf5abeb1b7abf0
gabrielestefono <gabrielestefono@hotmail.com>
2023-04-12 11:44:00 -0300

    Criando Lógica JavaScript

--- js/script.js
+++ js/script.js
@@ -1,16 +1,7 @@
 let mostrador = document.getElementById('mostrador');
-let historico = document.getElementById('historico');
-let  apagar = document.getElementById('apagar');
-let  inverter = document.getElementById('inverter');
-let  divisao = document.getElementById('divisao');
-let  porcentagem = document.getElementById('porcentagem');
-let  vezes = document.getElementById('vezes');
-let  menos = document.getElementById('menos');
-let  mais = document.getElementById('mais');
-let  ponto = document.getElementById('ponto');
-let  igual = document.getElementById('igual');
-let  operacoes = document.getElementsByClassName('operacoes');
-
+let apagar = document.getElementById('apagar');
+let ponto = document.getElementById('ponto');
+let mais = document.getElementById('mais');
 let btn0 = document.getElementById('btn-0');
 let btn1 = document.getElementById('btn-1');
 let btn2 = document.getElementById('btn-2');
@@ -21,49 +12,30 @@ let btn6 = document.getElementById('btn-6');
 let btn7 = document.getElementById('btn-7');
 let btn8 = document.getElementById('btn-8');
 let btn9 = document.getElementById('btn-9');
+let numero1 = 0;
+let numero2 = 0;
 
-mostrador.innerText =  0;
+let botoesNumeros = new Array(btn0,btn1,btn2,btn3,btn4,btn5,btn6,btn7,btn8,btn9);
+colocarPonto = false;
+mostrador.innerText = 0;
 
-let operacao1 = 0;
 
-let numeros = new Array(btn0,btn1,btn2,btn3,btn4,btn5,btn6,btn7,btn8,btn9);
 
-numeros.forEach(numero => {
+let mostrar = (valor)=>{
+    
+}
+
+botoesNumeros.forEach(numero=>{
     numero.addEventListener('click', ()=>{
-        if(mostrador.innerText == 0 & historico.innerText == ''){
-            mostrador.innerText = numero.textContent;
-        }else if(mostrador.innerText != 0 & operacao1 == 0){
-            mostrador.innerText = mostrador.innerText + numero.textContent;
-        }else if(mostrador.innerText == 0 & operacao1 != 0){
-            mostrador.innerText = numero.textContent;
-        }else if(mostrador.innerText != 0 & operacao1 != 0){
-            mostrador.innerText = mostrador.innerText + numero.textContent;
-        }
+        mostrar(numero.innerText)
     })
-});
+})
 
 apagar.addEventListener('click', ()=>{
     mostrador.innerText = 0;
-    historico.innerText = '';
 })
 
-inverter.addEventListener('click', ()=>{
-    if(mostrador.innerHTML > 0){
-        mostrador.innerHTML = mostrador.innerHTML * -1;
-    }else{
-        mostrador.innerHTML = mostrador.innerHTML * -1;
-    }
-})
-
-Array.from(operacoes).forEach((operacao)=>{
-    operacao.addEventListener('click', ()=>{
-        let criarHistorico = mostrador.innerHTML;
-        if(operacao1 == 0){
-            historico.innerHTML = mostrador.innerHTML + operacao.innerHTML;
-            operacao1++;
-            mostrador.innerHTML = 0
-        }else if(operacao1 == 1 ){
-            historico.innerHTML = criarHistorico + operacao.innerHTML;
-        }
-    })
+ponto.addEventListener('click', ()=>{
+    mostrar(ponto.innerText);
+    colocarPonto = true;
 })
